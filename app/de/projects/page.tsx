02c9203d0d48cae8ee0699e2fd8e1b6a09e54bd4
rem0ulade <jonathan.kokalj@gmail.com>
2026-07-20 12:44:16 +0200

    Use premium German projects page

--- app/de/projects/page.tsx
+++ app/de/projects/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { LocalizedServicePage } from '@/components/LocalizedServicePage'
+import { LocalizedProjectsPage } from '@/components/LocalizedProjectsPage'
 
 export const metadata: Metadata = {
   title: 'Projekte | Vantura Studios',
@@ -8,5 +8,5 @@ export const metadata: Metadata = {
 }
 
 export default function GermanProjectsPage() {
-  return <LocalizedServicePage locale="de" type="project" />
+  return <LocalizedProjectsPage locale="de" />
 }
